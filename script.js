b682f41da93b1b463b6ec222eaabaf19b2f9c10a
ZMFshadow <andremadeirafigueiredo@outlook.com>
2025-03-16 00:06:50 +0000

    Initial commit with calculator files

---
+++ script.js
@@ -0,0 +1,33 @@
+let display = document.getElementById('display');
+let currentInput = '0';
+
+function insert(value) {
+    if (currentInput === '0' && value !== '.') {
+        currentInput = value;
+    } else {
+        currentInput += value;
+    }
+    display.innerText = currentInput;
+}
+
+function clearDisplay() {
+    currentInput = '0';
+    display.innerText = currentInput;
+}
+
+function deleteLast() {
+    currentInput = currentInput.slice(0, -1);
+    if (currentInput === '') {
+        currentInput = '0';
+    }
+    display.innerText = currentInput;
+}
+
+function calculate() {
+    try {
+        currentInput = eval(currentInput).toString();
+        display.innerText = currentInput + ' €';
+    } catch (error) {
+        display.innerText = 'Erro';
+    }
+}
