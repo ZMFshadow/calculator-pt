ffca0cbbedd8054765056b201731145d93f90b86
ZMFshadow <andremadeirafigueiredo@outlook.com>
2025-03-16 23:20:40 +0000

    Commit to main

--- script.js
+++ script.js
@@ -17,36 +17,43 @@ function calculateCommissions() {
     const agentsTier1 = document.getElementById('agentsTier1').value;
     const commissionTier1 = (avgHomePrice * (commissionPercentage / 100)) * agentsTier1 * 0.035;
     totalAnnualShare += commissionTier1;
+    document.getElementById('tier1Income').textContent = commissionTier1.toFixed(2);
 
     // Cálculo para o Tier 2
     const agentsTier2 = document.getElementById('agentsTier2').value;
     const commissionTier2 = (avgHomePrice * (commissionPercentage / 100)) * agentsTier2 * 0.002;
     totalAnnualShare += commissionTier2;
+    document.getElementById('tier2Income').textContent = commissionTier2.toFixed(2);
 
     // Cálculo para o Tier 3
     const agentsTier3 = document.getElementById('agentsTier3').value;
     const commissionTier3 = (avgHomePrice * (commissionPercentage / 100)) * agentsTier3 * 0.001;
     totalAnnualShare += commissionTier3;
+    document.getElementById('tier3Income').textContent = commissionTier3.toFixed(2);
 
     // Cálculo para o Tier 4
     const agentsTier4 = document.getElementById('agentsTier4').value;
     const commissionTier4 = (avgHomePrice * (commissionPercentage / 100)) * agentsTier4 * 0.001;
     totalAnnualShare += commissionTier4;
+    document.getElementById('tier4Income').textContent = commissionTier4.toFixed(2);
 
     // Cálculo para o Tier 5
     const agentsTier5 = document.getElementById('agentsTier5').value;
     const commissionTier5 = (avgHomePrice * (commissionPercentage / 100)) * agentsTier5 * 0.001;
     totalAnnualShare += commissionTier5;
+    document.getElementById('tier5Income').textContent = commissionTier5.toFixed(2);
 
     // Cálculo para o Tier 6
     const agentsTier6 = document.getElementById('agentsTier6').value;
     const commissionTier6 = (avgHomePrice * (commissionPercentage / 100)) * agentsTier6 * 0.005;
     totalAnnualShare += commissionTier6;
+    document.getElementById('tier6Income').textContent = commissionTier6.toFixed(2);
 
     // Cálculo para o Tier 7
     const agentsTier7 = document.getElementById('agentsTier7').value;
     const commissionTier7 = (avgHomePrice * (commissionPercentage / 100)) * agentsTier7 * 0.005;
     totalAnnualShare += commissionTier7;
+    document.getElementById('tier7Income').textContent = commissionTier7.toFixed(2);
 
     // Calcular valores anual e mensal
     const totalMonthlyShare = totalAnnualShare / 12;
